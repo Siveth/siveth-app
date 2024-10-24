079f313f5dcc3c2d13a4033515e921ee89ce861a
AlanHH28 <alher2803@gmail.com>
2024-10-23 18:59:01 -0600

    header rout name

--- src/Componentes/Usuario/header.jsx
+++ src/Componentes/Usuario/header.jsx
@@ -349,7 +349,7 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Viajes ramos</span>
               <img className="h-25 w-auto" src="/src/img/logo.png" alt="" />
             </a>
             <button
